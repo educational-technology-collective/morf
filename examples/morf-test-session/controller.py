e86206826b2d225eb3fd6b4141fe273c1ec9c1b4
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-03-30 12:57:11 -0400

    completed testing version

--- examples/morf-test-session/controller.py
+++ examples/morf-test-session/controller.py
@@ -29,8 +29,8 @@ from morf.workflow.test import test_course
 from morf.workflow.evaluate import evaluate_course
 
 
-extract_session(multithread = False)
-# extract_holdout_session()
-# train_session(label_type = 'dropout')
-# test_course()
-# evaluate_course(label_type = 'dropout')
+extract_session()
+extract_holdout_session()
+train_session(label_type = 'dropout')
+test_course(label_type = 'dropout')
+evaluate_course(label_type = 'dropout')
