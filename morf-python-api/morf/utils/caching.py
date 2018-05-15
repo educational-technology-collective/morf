00034250a2e6f0a4cac0c38b50cd5860e36fb3a6
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-05-15 16:51:05 -0400

    updated caching to usejob_config object directly and to read path to aws executable from job_config object

--- morf-python-api/morf/utils/caching.py
+++ morf-python-api/morf/utils/caching.py
@@ -30,27 +30,29 @@ import logging
 
 logger = logging.getLogger()
 
-def cache_s3_to_local(bucket, local_dest):
+def cache_s3_to_local(job_config, bucket):
     """
-    Cache all data in an s3 bucket to local_dest, creating a complete copy of files and directory structure.
+    Cache all data in an s3 bucket to job_config.cache_dir, creating a complete copy of files and directory structure.
+    :param job_config: MorfJobConfig object.
     :param bucket: path to s3 bucket.
-    :param local_dest: local destination to cache to (string). If it does not exist, it will be created.
     :return:
     """
-    # check paths
-    s3_url = urlparse(bucket)
-    assert s3_url.scheme == "s3", "specify a valid path to an s3 bucket"
-    # create local_dest directory if not exists
-    if not os.path.exists(local_dest):
+    s3bucket = "s3://{}".format(bucket)
+    bucket_cache_dir = os.path.join(job_config.cache_dir, bucket)
+    # create job_config.cache_dir directory if not exists
+    if not os.path.exists(job_config.cache_dir):
         try:
-            os.makedirs(local_dest)
+            os.makedirs(job_config.cache_dir)
         except exception as e:
             logger.error("error creating cache: {}".format(e))
             raise
     # execute s3 sync command
-    cmd = "aws s3 sync {} {}".format(bucket, local_dest)
+    cmd = "{} s3 sync {} {}".format(job_config.aws_exec, s3bucket, bucket_cache_dir)
     logger.info("running {}".format(cmd))
-    subprocess.call(cmd, shell=True)
+    try:
+        subprocess.call(cmd, shell=True)
+    except Exception as e:
+        logger.warning("exception when executing sync: {}".format(e))
     return
 
 
@@ -60,10 +62,7 @@ def update_morf_job_cache(job_config):
     :param job_config: MorfJobConfig object.
     :return:
     """
-    job_cache_dir = job_config.cache_dir
     # cache each bucket in a named directory within job_cache_dir
     for raw_data_bucket in job_config.raw_data_buckets:
-        s3bucket = "s3://{}".format(raw_data_bucket)
-        bucket_cache_dir = os.path.join(job_cache_dir, raw_data_bucket)
-        cache_s3_to_local(s3bucket, bucket_cache_dir)
+        cache_s3_to_local(job_config, raw_data_bucket)
     return
