1e89c02745f8de447f4b37592d3d96738c9a05c6
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-09-04 16:37:04 -0700

    initial commit; controller for prule mwe

---
+++ mwe-prule/controller.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2018 The Regents of the University of Michigan
+# and the University of Pennsylvania
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+A test script to extract features, train a model, and evaluate it using the MORF 2.0 architecture.
+"""
+
+from morf.workflow.extract import extract_session, extract_holdout_session
+from morf.workflow.evaluate import evaluate_prule_session
+
+extract_session()
+extract_holdout_session()
+evaluate_prule_session()
