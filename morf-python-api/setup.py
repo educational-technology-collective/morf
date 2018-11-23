d8edee1e8edf1d0585ea79a0dc51f33d8f723bf9
jpgard <joshua.patrick.gardner@gmail.com>
2018-11-23 10:27:50 -0500

    incremented version to 0.5; added basic testing infrastructure

--- morf-python-api/setup.py
+++ morf-python-api/setup.py
@@ -61,7 +61,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.17',  # see https://semver.org/
+    version='0.5.0',  # see https://semver.org/
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -158,5 +158,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['pandas', 'sklearn', 'boto3', 'boto', 'scipy'],  # Optional
     python_requires='>=3',
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
 
 )
